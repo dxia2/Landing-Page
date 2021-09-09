e5f43c1c5ccffd5d7fa95c422d38137dcdfebf99
dxia2 <90279328+dxia2@users.noreply.github.com>
2021-09-09 12:43:11 -0600

    Add files via upload

---
+++ LandingPage/script.js
@@ -0,0 +1,9 @@
+// landing page by david
+
+//prompt user for data
+let name = prompt("What is your name?");
+let focus = prompt("What is your main focus for today?");
+
+//output
+document.getElementById("nameSpan").innerHTML = name;
+document.getElementById("focusP").innerHTML = focus;
